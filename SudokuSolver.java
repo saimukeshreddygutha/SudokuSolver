3d66b0fcba01aba5fbdd3d7310eb54c7c2cf3da2
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-08-09 10:43:52 +0530

    added comments

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -18,6 +18,7 @@ public class SudokuSolver {
 			if(sudoku[i][k] == num || sudoku[k][j] == num)return false;
 		}
 
+		// to get the starting index of the subgrid
 		int sx = (i/3)*3;
 		int sy = (j/3)*3;
 
@@ -32,23 +33,32 @@ public class SudokuSolver {
 
 	public static boolean solve(int[][] sudoku, int i, int j, int n){
 
+		// check if i == n -> all rows from 0 to 8 are filled hence print the sudoku
+
 		if(i == n){
 			printSudoku(sudoku, n);
 			return true;
 		}
 
+
+		// check if j reached 9 > 8 and return next subproblem
 		if(j == n)return solve(sudoku, i + 1, 0, n);
 
 		if(sudoku[i][j] != 0)return solve(sudoku, i, j + 1, n);
 
+		// iterate num from 1 to 9 and check if it is safe to place the num
 		for(int num = 1;num <= 9;num++){
 			if(isSafe(sudoku, num, i, j, n)){
+				// if safe update the number and check if the subproblem can be solved
 				sudoku[i][j] = num;
+				// if the sub problem can be solved then only return true
+				// donot return solve(sudoku, i, j + 1, n); it ends the next steps
 				boolean isSolveable = solve(sudoku, i, j + 1, n);
 				if(isSolveable == true)return true;
 			}
 		}
 
+		// if there is no chance to place any number we change it back to zero and return false
 		sudoku[i][j] = 0;
 		return false;
 	}
